acceef485c20b4ef3daea00d4d1c4153e6802a28
JamieOnSootMC <jamie@sootmc.net>
2023-08-21 12:44:20 +0100

    Replace maven with gradle

--- src/main/java/net/sootmc/exampleplugin/Example_plugin.java
+++ src/main/java/net/sootmc/exampleplugin/Example_plugin.java
@@ -2,7 +2,7 @@ package net.sootmc.exampleplugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ExamplePlugin extends JavaPlugin {
+public final class Example_plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
